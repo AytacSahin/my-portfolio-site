447e3be9213fecb8ad84a3384a6078dd97af4278
AytacSahin <haytacsahin@gmail.com>
2023-06-05 00:57:35 +0300

    starting styling within componets

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,10 @@ import HeaderButtons from '../sources/buttons/HeaderButtons'
 
 const Header = () => {
   return (
-    <div className=' flex flex-row justify-between'>
+    <div className='flex justify-between'>
 
-      <div className='text-left'>
+      <div className='w-[70%] flex flex-col justify-between items-start'>
+
         <h4>----------Aytaç Şahin</h4>
         <h1>Creative thinker Minimalism lover</h1>
         <p>Hi, I'm Aytac Sahin. I'm a full stack developer. If you are looking for a Developer who to craft solid and scalable frontend products with graeat user experiences. Let's shake hands with me.</p>
@@ -17,7 +18,7 @@ const Header = () => {
 
       </div>
 
-      <div>
+      <div >
         <img className='rounded-lg' src={headerImage} alt="profile_picture"></img>
       </div>
 

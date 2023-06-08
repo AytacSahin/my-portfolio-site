c09f1ab57532b2bd250d5664c495ec886c2b2018
AytacSahin <haytacsahin@gmail.com>
2023-06-08 03:25:41 +0300

    test

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
-import { projectsDataEn, projectsDataTr } from '../sources/data/projectsData'
+import { projectsDataEn, projectsDataTr } from '../sources/data/projectsData.js'
 
 const Projects = () => {
 

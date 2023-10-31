d1fb091fe41f9526f5101d25657ca9c651e95e1d
AytacSahin <haytacsahin@gmail.com>
2023-10-31 19:27:48 +0300

    project links updated

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,18 +19,19 @@ const Projects = () => {
                             <div key={index} className='w-[20rem] h-[36rem] mx-auto flex flex-col justify-between mb-12 sm:w-[20rem] sm:h-[36rem] max-[640px]:w-[20rem] max-[640px]:h-[40rem]'>
 
                                 <div className='mb-8'>
-                                    <img className='hover:scale-110 transition duration-500 cursor-pointer w-[20rem] h-[14rem] object-fill hover:opacity-70' src={item.img} alt="project_picture"></img>
+                                    <a href={item.deployLink}>
+                                        <img className='hover:scale-110 transition duration-500 cursor-pointer w-[20rem] h-[14rem] object-fill hover:opacity-70' src={item.img} alt="project_picture"></img>
+                                    </a>
                                     <h3 className='text-[#4338CA] text-3xl leading-7 font-medium mt-8 dark:text-[#B7AAFF] mb-6'>{item.title}</h3>
                                     <h4 className='text-[#6B7280] text-sm leading-tight font-normal dark:text-[#FFFFFF]'>{item.description}</h4>
                                 </div>
-                                
+
                                 <div>
                                     <div className='mb-4 flex sm:flex-row max-[640px]:flex-col max-[640px]:gap-1'>
                                         {item.methodsofproject.map((item, index) => (
                                             <span key={index} className='text-[#3730A3] border-2 border-solid rounded-md border-[#3730A3] p-2 mr-4 dark:text-[#8F88FF] dark:border-[#8F88FF dark:bg-[#383838]'>
                                                 {item}
-                                            </span>
-                                        ))}
+                                            </span>))}
                                     </div>
 
                                     <div className='flex justify-between mb-4 underline text-[#3730A3] dark:text-[#E1E1FF]'>
